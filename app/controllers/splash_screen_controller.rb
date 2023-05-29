11c54f4c94c16a25e799fd60b529be45c5baa0ea
mrprotocoll <lekanvgbg@gmail.com>
2023-05-29 19:05:27 +0100

    Add boostrap to application layout

--- app/controllers/splash_screen_controller.rb
+++ app/controllers/splash_screen_controller.rb
@@ -1,2 +1,4 @@
 class SplashScreenController < ApplicationController
+    def index
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "splash#index"
+  root "splash_screen#index"
 end
