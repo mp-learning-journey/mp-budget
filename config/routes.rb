03175560111b3b80d7745c45dcc843c9060abbdb
mrprotocoll <lekanvgbg@gmail.com>
2023-05-30 16:44:37 +0100

    Add group to route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :groups
   # Defines the root path route ("/")
-  root "splash_screen#index"
+
+  authenticated :user do
+    root 'groups#index', as: :authenticated_root
+  end
+
+  unauthenticated do
+    root "splash_screen#index", as: :unauthenticated_root
+  end
 end
