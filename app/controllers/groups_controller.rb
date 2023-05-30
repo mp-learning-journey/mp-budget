34efd6a55e37fa9861a600497b7f063eaa15d994
mrprotocoll <lekanvgbg@gmail.com>
2023-05-30 16:57:24 +0100

    Add group controller actions for new group and all groups by cuurent user

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,16 @@
 class GroupsController < ApplicationController
+
+    def index
+        @groups = current_user.groups
+    end
+
+    def new
+        @groups = Group.new
+    end
+
+    protected
+
+    def set_groups
+        @groups = Group.find(params[:id])
+    end
 end
