85c5ec6495c092dbeab16a2e925dee78cdc9e094
mrprotocoll <lekanvgbg@gmail.com>
2023-05-31 19:40:01 +0100

    Add active storage

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,9 +8,22 @@ class GroupsController < ApplicationController
         @groups = Group.new
     end
 
+    def create
+        @group = Group.new(group_params)
+        if @group.save
+            redirect_to authenticated_root_path, notice: "Category created successful"
+        else
+            render :new
+        end
+    end
+
     protected
 
     def set_groups
         @groups = Group.find(params[:id])
+    end
+    
+    def group_params
+        params.require(:group).permit(:name, :icon)
     end
 end
