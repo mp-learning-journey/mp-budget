e97d22679b87bfc62aa8efd5715a7c924d96a41d
mrprotocoll <lekanvgbg@gmail.com>
2023-05-31 23:59:01 +0100

    fix linters

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,26 +1,25 @@
 class PurchasesController < ApplicationController
+  def index
+    @groups = Purchase.find_by('group_id', params[:id])
+  end
 
-    def index
-        @groups = Purchase.find_by("group_id", params[:id])
-    end
-    
-    def new
-        @id = params[:group_id]
-        @purchase = Purchase.new
-    end
-    
-    def create
-        @purchase = current_user.purchases.new(purchase_params)
-        if @purchase.save
-          redirect_to authenticated_root_path, notice: 'Category created successful'
-        else
-          render :new
-        end
-    end
-    
-    protected
+  def new
+    @id = params[:group_id]
+    @purchase = Purchase.new
+  end
 
-    def purchase_params
-        params.require(:purchase).permit(:group, :icon)
+  def create
+    @purchase = current_user.purchases.new(purchase_params)
+    if @purchase.save
+      redirect_to authenticated_root_path, notice: 'Category created successful'
+    else
+      render :new
     end
+  end
+
+  protected
+
+  def purchase_params
+    params.require(:purchase).permit(:group, :icon)
+  end
 end
