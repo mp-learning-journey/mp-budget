dd0b88b413676a99397e86dc6c5925f17417f417
mrprotocoll <lekanvgbg@gmail.com>
2023-05-31 23:08:32 +0100

    Add index mew and create action for purchases

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,2 +1,26 @@
 class PurchasesController < ApplicationController
+
+    def index
+        @groups = Purchase.find_by("group_id", params[:id])
+    end
+    
+    def new
+        @group = current_user.groups
+        @purchase = Purchase.new
+    end
+    
+    def create
+        @purchase = current_user.purchases.new(purchase_params)
+        if @purchase.save
+          redirect_to authenticated_root_path, notice: 'Category created successful'
+        else
+          render :new
+        end
+    end
+    
+    protected
+
+    def purchase_params
+        params.require(:purchase).permit(:group, :icon)
+    end
 end
