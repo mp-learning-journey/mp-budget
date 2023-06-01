bf580c32ae2226906e8313e5dca084211f40ce0b
mrprotocoll <lekanvgbg@gmail.com>
2023-06-01 12:52:26 +0100

    skip aunthentication for splash screen

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :groups do
-    resources :purchases
+    resources :purchases, only: [:new, :create]
   end
   # Defines the root path route ("/")
 
